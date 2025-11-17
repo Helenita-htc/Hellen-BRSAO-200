d77a9b1cd1b53dcf0ffbaacf69be2342e9fa5fee
Hellen Thammier <hellent.unb@gmail.com>
2025-11-17 12:36:20 -0300

    Mensagem do commit

--- atividadespraticas02/questao01.py
+++ atividadespraticas02/questao01.py
@@ -5,15 +5,14 @@ Crie um programa que converte um valor em reais para dólares e euros. Use os se
 * Taxa do dólar: R$ 5.20
 * Taxa do euro: R$ 6.15
 O programa deve calcular e exibir os valores convertidos, arredondando para duas casas decimais."""
-real= 100.00
-taxadoDolar= 5.20
-taxadoeuro= 6.15
 
-dolar= real * taxadoDolar 
-euro= real * taxadoeuro
+real = 100.00
+taxaDolar = 5.20
+taxaEuro = 6.15
+
+dolar = real * taxaDolar
+euro = real * taxaEuro
 
 print("Tenho","R$",real)
 print("Em dolar tenho: $",dolar)
-print("Em Euro tenho: €",euro)
-
-
+print("Em Euro tenho: €", euro)
